601a8879ede518a54dc19d52535822cb119dd7b8
yhc <didguscjf95@naver.com>
2021-05-05 00:51:02 +0900

    test

--- gongmoju/settings/common.py
+++ gongmoju/settings/common.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-v#4n82whjwf*m)fzr#^oxyd@pru5d74u9g15c+s%2t%^eo@h!j'
-SECRET_KEY = os.environ.get("DJANGO_SECRET")
+SECRET_KEY = 'django-insecure-v#4n82whjwf*m)fzr#^oxyd@pru5d74u9g15c+s%2t%^eo@h!j'
+# SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
