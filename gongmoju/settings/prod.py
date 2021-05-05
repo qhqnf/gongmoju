f61304168ee73febb058ca2f75d2a39e50767f9e
yhc <didguscjf95@naver.com>
2021-05-05 19:19:58 +0900

    test for production

--- gongmoju/settings/prod.py
+++ gongmoju/settings/prod.py
@@ -1,3 +1,13 @@
 from .common import *
 
 DEBUG = os.environ.get("DEBUG") in ["True", "t", "TRUE", "1"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ["DB_HOST"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "NAME": os.environ.get("DB_NAME", "postgres"),
+    }
+}
