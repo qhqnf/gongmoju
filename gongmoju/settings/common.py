49f36db263d315412981eecd757f3ca72318807b
yhc <didguscjf95@naver.com>
2021-05-07 23:07:25 +0900

    modify static root

--- gongmoju/settings/common.py
+++ gongmoju/settings/common.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT  = BASE_DIR / STATIC_URL
+STATIC_ROOT  = os.path.join(BASE_DIR, "static")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
